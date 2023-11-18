de6fba29609e02000bd874a511d061a95720f77f
Luis Ramirez <lp.ramirez.duarte.7@gmail.com>
2023-11-18 16:03:33 +0100

    chore(api): Update allowed hosts

--- blog/settings.py
+++ blog/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["blog-api-production-760e.up.railway.app"]
+ALLOWED_HOSTS = ["blog-api-production-760e.up.railway.app", "localhost"]
 CSRF_TRUSTED_ORIGINS = ["https://blog-api-production-760e.up.railway.app"]
 
 
